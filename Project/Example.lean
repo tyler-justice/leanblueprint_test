4d588b1ef0c4395763202af4f0b27e229a31eb0e
MATSUSHITA Daisuke <matusita@math.sci.hokudai.ac.jp>
2025-06-26 12:10:32 +0900

    Doc が上手くいくか確認してみる

--- Project/Example.lean
+++ Project/Example.lean
@@ -1,1 +1,19 @@
-import Mathlib.LinearAlgebra.QuadraticForm.Basic
+import Mathlib
+
+theorem card (n : ℕ) [Fintype (ZMod n)] : Fintype.card (ZMod n) = n := by
+  cases n with
+  | zero => exact (not_finite (ZMod 0)).elim
+  | succ n => convert Fintype.card_fin (n + 1) using 2
+
+theorem card_units (p : ℕ) [Fact p.Prime] : Fintype.card (ZMod p)ˣ = p - 1 := by
+  rw [Fintype.card_units, card]
+
+theorem katabami_theorem_fermat1 (p : ℕ) [Fact p.Prime] {a : ZMod p} (ha : a ≠ 0) :
+    a ^ (p - 1) = 1 := by
+  have h := FiniteField.pow_card_sub_one_eq_one a ha
+  rwa [ZMod.card p] at h
+
+theorem katabami_theorem_fermat2 (p : ℕ) [Fact p.Prime] (a : (ZMod p)ˣ) : a ^ (p - 1) = 1 := by
+  rw [← card_units p, pow_card_eq_one]
+
+#min_imports
